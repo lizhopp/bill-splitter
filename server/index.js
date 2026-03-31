02fad45b4dd3551536115bb42b5a1139360ddb1c
Liz Hoppstetter <liz.hoppstetter@fullstackacademy.com>
2026-03-30 20:48:54 -0500

    seed endpoint

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import { getGreeting } from "./db/queries/greetings.js";
-import express from "express"
+import express from "express";
 import db from "./db/client.js";
 import cors from "cors";
 
@@ -12,10 +12,23 @@ app.use(cors());
 
 app.get("/greet", helloWorld);
 
+app.get("/seed", seed);
+
+async function seed(req, res) {
+  await db.query(`CREATE TABLE IF NOT EXISTS greetings(
+        id SERIAL PRIMARY KEY,
+        message TEXT NOT NULL);
+        `);
+
+  await db.query(`INSERT INTO greetings (message)
+    VALUES('Hello World');`);
+
+  res.send("database seeded");
+}
 async function helloWorld(req, res) {
   try {
-    console.log("calling db getGreeting")
-    const greeting = await getGreeting()
+    console.log("calling db getGreeting");
+    const greeting = await getGreeting();
     res.status(200).json(greeting);
   } catch (error) {
     console.log("error");
